a6b81da5ef94d56293cf8d0522afde70dace8f62
Bruno Santos Ichikawa <149208859+BrunoIchikawa@users.noreply.github.com>
2024-08-06 17:28:12 -0300

    Add files via upload

---
+++ Baskhara/js/script.js
@@ -0,0 +1,6 @@
+function calcular(){
+    var a = document.getElementById('n1').value;
+    var b = document.getElementById('n2').value;
+    var c = document.getElementById('n3').value;
+    var delta,x1,x2;
+}
